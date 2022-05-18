f45787ca4696e516a6b89b66025346f14445b5a4
Aibar Hernan Ezequiel <aibarhernanezequiel@gmail.com>
2022-05-17 21:15:56 -0300

    Setup proyect

---
+++ bingo-kata/src/main/java/com/aibar/bingokata/BingoKataApplication.java
@@ -0,0 +1,13 @@
+package com.aibar.bingokata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BingoKataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BingoKataApplication.class, args);
+	}
+
+}
---
+++ bingo-kata/src/test/java/com/aibar/bingokata/BingoKataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aibar.bingokata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BingoKataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
